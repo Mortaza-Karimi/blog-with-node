48a5abcbcd92a600434b6e963b3c75b12a5afcd8
Mortaza-Karimi <salambarf@gmail.com>
2021-08-28 17:28:02 +0430

    fix show categories bug

--- app/categories/showCategories.js
+++ app/categories/showCategories.js
@@ -1,13 +1,8 @@
 const fs = require("fs");
 const showCategories = (req, res) => {
-  const categories = { categories: [] };
-
-  const dirFileList = fs.readdirSync("articles");
-
-  dirFileList.map(function (value, index) {
-    const thisArticle = fs.readFileSync(`articles/${value}/data.json`, "utf-8");
-    categories.categories.push(JSON.parse(thisArticle)["category"]);
-  });
+  const categories = JSON.parse(
+    fs.readFileSync("./app/categories/categories.json", "utf-8")
+  );
 
   res.json(categories);
 };
