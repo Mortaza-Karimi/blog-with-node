23422237011f6b14f550d5ac4d758b4175a48ba1
Mortaza-Karimi <salambarf@gmail.com>
2021-09-01 11:18:14 +0430

    change write if algoritm

--- app/articles/addArticle.js
+++ app/articles/addArticle.js
@@ -3,36 +3,37 @@ const addArticle = (req, res) => {
   console.log(req.body["name"]);
   console.log(req.body.name);
   if (
-    req.body.name &&
-    req.body.category &&
-    req.body.tags &&
-    req.body.year &&
-    req.body.month &&
-    req.body.content
+    !req.body.name &&
+    !req.body.category &&
+    !req.body.tags &&
+    !req.body.year &&
+    !req.body.month &&
+    !req.body.content
   ) {
-    fs.mkdirSync(`./articles/${req.body.name}`);
-    fs.writeFileSync(
-      `./articles/${req.body.name}/${req.body.name}.md`,
-      req.body.content
-    );
-    const jsonData = {
-      name: req.body.name,
-      category: req.body.category,
-      tags: req.body.tags,
-      date: {
-        year: req.body.year,
-        month: req.body.month,
-      },
-    };
-    console.log(jsonData);
-    fs.writeFileSync(
-      `./articles/${req.body.name}/data.json`,
-      JSON.stringify(jsonData)
-    );
+    res.json({ status: "fail" });
 
-    res.json({ status: "ok" });
     return;
   }
-  res.json({ status: "fail" });
+  fs.mkdir(`./articles/${req.body.name}`);
+  fs.writeFileSync(
+    `./articles/${req.body.name}/${req.body.name}.md`,
+    req.body.content
+  );
+  const jsonData = {
+    name: req.body.name,
+    category: req.body.category,
+    tags: req.body.tags,
+    date: {
+      year: req.body.year,
+      month: req.body.month,
+    },
+  };
+  console.log(jsonData);
+  fs.writeFile(
+    `./articles/${req.body.name}/data.json`,
+    JSON.stringify(jsonData)
+  );
+
+  res.json({ status: "ok" });
 };
 module.exports = addArticle;
