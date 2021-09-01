1b8f224c6c755d8cf2250f39362dd1af7609aaea
Mortaza-Karimi <salambarf@gmail.com>
2021-09-01 11:18:48 +0430

    change writing if algoritm

--- app/categories/addCategory.js
+++ app/categories/addCategory.js
@@ -1,20 +1,20 @@
 const { json } = require("body-parser");
 const fs = require("fs");
 const addCategory = (req, res) => {
-  if (req.body.category) {
-    var categories = JSON.parse(
-      fs.readFileSync(`./app/categories/categories.json`, "utf-8")
-    );
-    categories.categories.push(req.body.category);
-    console.log(categories.categories);
-    fs.writeFileSync(
-      `./app/categories/categories.json`,
-      JSON.stringify(categories)
-    );
-
-    res.json({ status: "ok" });
+  if (!req.body.category) {
+    res.json({ status: "fail" });
     return;
   }
-  res.json({ status: "fail" });
+  var categories = JSON.parse(
+    fs.readFileSync(`./app/categories/categories.json`, "utf-8")
+  );
+  categories.categories.push(req.body.category);
+  console.log(categories.categories);
+  fs.writeFileSync(
+    `./app/categories/categories.json`,
+    JSON.stringify(categories)
+  );
+
+  res.json({ status: "ok" });
 };
 module.exports = addCategory;
