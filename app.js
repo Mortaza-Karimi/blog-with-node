f7f9e3baff33b0210cae4f61b8c54d73e86e7eda
Mortaza-Karimi <salambarf@gmail.com>
2021-09-02 15:13:15 +0430

    add client routs

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const categories = require("./app/server/categories/index");
 const dates = require("./app/server/dates/index");
 const tags = require("./app/server/tags/index");
 
+const client_articles = require("./app/client/articles/index");
+const client_categories = require("./app/client/categories/index");
+const client_dates = require("./app/client/dates/index");
+const client_tags = require("./app/client/tags/index");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -29,7 +34,7 @@ app.get("/api/article/:article", articles.showArticle);
 app.post("/api/articles", articles.addArticle);
 app.put("/api/articles", articles.updateArticle);
 app.patch("/api/articles", articles.updateArticle);
-app.delete("/api/articles", articles.deleteArticle);
+app.delete("/api/articles/:article", articles.deleteArticle);
 
 // Category routs
 app.get("/api/categories/:category", categories.showByCategories);
@@ -45,6 +50,31 @@ app.get("/api/years/:year", dates.showByYear);
 app.get("/api/years", dates.showYears);
 app.get("/api/years/:year/:month", dates.showByMonth);
 app.get("/api/months", dates.showMonths);
+
+// Client routs
+
+// Artciel routs
+app.get("/articles", client_articles.showArticles);
+app.get("/article/:article", client_articles.showArticle);
+app.post("/articles", client_articles.addArticle);
+app.put("/articles", client_articles.updateArticle);
+app.patch("/articles", client_articles.updateArticle);
+app.delete("/articles/:article", client_articles.deleteArticle);
+
+// Category routs
+app.get("/categories/:category", client_categories.showByCategories);
+app.get("/categories", client_categories.showCategories);
+app.post("/categories", client_categories.addCategory);
+
+// Tag routs
+app.get("/tags", client_tags.showTags);
+app.get("/tags/:tag", client_tags.showByTag);
+
+// Date routs
+app.get("/years/:year", client_dates.showByYear);
+app.get("/years", client_dates.showYears);
+app.get("/years/:year/:month", client_dates.showByMonth);
+app.get("/months", client_dates.showMonths);
 
 app.listen(2000);
 console.log("\n\nListining on : http://localhost:2000\n\n");
